87918501f65a1b148aca125e006e76867ef9b5b5
vimalmshetty <vimalmshetty@gmail.com>
2014-12-18 07:37:44 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignemnt 2 - R Programming
+## Vimal Kumra M / 18.12.2014
+## Matrix inverse is a costly function, Hence we are trying to cache the inverse of the matrix to avoid repeated calculation
 
 ## Write a short comment describing this function
 
